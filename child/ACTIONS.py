93fc8dd55b2a494594de329d3080d4a8b7d47564
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2022-02-17 05:53:08 +0530

    feat(child): new actions
    Modify `dispatch_action()` to integrate the new dispatcher.exe
    to cooperate with dynamic cmd exec via new constants defined
    in `ACTIONS.py`

---
+++ child/ACTIONS.py
@@ -0,0 +1,6 @@
+SHUTDOWN:int = 1
+LOGOFF:int = 2
+RESTART:int = 3
+EXEC_CMD:int = 4
+EXEC_TASK:int = 5
+HEALTH_CHECK:int = 6
--- child/commands.py
+++ child/commands.py
@@ -1,10 +1,33 @@
+# Imports
+import sys
 import subprocess as sp
 import os
 
-cwd:str = os.getcwd()
 
-async def shutdown():
-    sp.Popen(
-        args="dispatcher.exe",
-        cwd=cwd
-    )
+# BEGIN
+
+# global vars
+cwd:str = os.getcwd()
+dispatcher_bin:str = "dispatcher.exe"
+
+
+async def dispatch_action(action:int) -> sp.CompletedProcess | None:
+    # use Popen if version of py interpreter < 3.5
+    
+    if sys.version_info.major < 3 and sys.version_info.minor < 5: 
+        sp.Popen(
+                args=f"{dispatcher_bin} {action}",
+                cwd=cwd
+        )
+    else:
+
+        dispatched:sp.CompletedProcess = sp.run(
+                                                args=f"{dispatcher_bin} {action}",
+                                                capture_output=True
+                                            )
+        return dispatched
+
+# END
+
+if __name__ == '__main__':
+    pass
